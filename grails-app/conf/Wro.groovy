f2deb3bb54aa7e8e4ba63088f4e4936679c226cc
genuinefafa <genuinefafa@gmail.com>
2013-07-28 20:59:11 -0300

    wro4j plugin, wro.groovy simple setup, classpath updates

---
+++ grails-app/conf/Wro.groovy
@@ -0,0 +1,10 @@
+groups {
+  all {
+	css(minimize: false, "/css/*.css")
+	css(minimize: false , "/less/*.less")
+    js "/js/*.js"
+  }
+  bootstrap {
+	css(minimize: false, "/less/lib/bootstrap/bootstrap.less")
+  }
+}
